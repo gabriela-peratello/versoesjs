78a45ed57c77c03248afcb58f5a02c25e9d67d1c
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-02-05 11:24:27 -0300

    Operacoes - Subtração e adição

---
+++ operadores-v0.js
@@ -0,0 +1,32 @@
+function Somar() {
+    
+    let v1 = parseFloat(prompt('valor01'));
+
+    alert(`O valor digitado foi ${v1}.`);
+    let v2 = parseFloat(prompt('valor02'));
+    alert(`O segundo valor digitado foi ${v2}.`)
+
+    document.getElementById('resultado').style.display = 'flex';
+    document.getElementById('resultado').textContent = `O resultado é ${v1 + v2}.`;
+};
+
+
+function Subtrair() {
+    let n1 = parseFloat(prompt('valor01'));
+    //template de str
+    alert(`O valor digitado foi ${n1}.`);
+    let n2 = parseFloat(prompt('valor02'));
+    alert(`O segundo valor digitado foi ${n2}.`)
+
+    document.getElementById('resultado').style.display = 'flex';
+    document.getElementById('resultado').textContent = `O resultado é ${n1} - ${n2}.`;
+
+};
+
+function Multiplicar() {
+
+};
+
+function Dividir() {
+
+};
